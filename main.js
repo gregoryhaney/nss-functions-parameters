2a8e6422abd760370507e709c2bffb1d1819380b
Gregory Haney <gregory.haney@gmail.com>
2022-01-12 11:50:55 -0600

    INITIAL: ** if a function returns a value, you MUST store it in a variable

---
+++ main.js
@@ -0,0 +1,16 @@
+
+const quarterValue = (originalNumber) => {
+
+        // new variable to hold the calculated result
+    const newNumber = (originalNumber / 4)
+        // return that result
+        // RECALL that if a function returns a value, it MUST
+        // be stored in a variable
+    return newNumber
+}
+
+    // Invoke the function and store the return value
+const answer = quarterValue(7340)
+
+// Log the return value
+console.log(answer)
